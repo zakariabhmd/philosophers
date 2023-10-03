6233400bf423ec6c391384df51b42928e3a41223
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-10-03 23:30:13 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 16:08:29 by zbabahmi          #+#    #+#             */
+/*   Updated: 2023/10/03 23:27:06 by zbabahmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "philo.h"
+int main(int ac, char **av)
+{
+	t_damage	*data;
+	t_savage	*philo;
+
+	if	(ac == 5 || ac == 6)
+	{
+		if (check_args(av))
+			return (1);
+		if(alloc_philo(data, &philo, av))
+			return (printf("khataa fi alloc\n", 1));
+	}
+	else
+	{
+		printf("khataa matba3i");
+		return(1);
+	}
+	return(0);
+}
